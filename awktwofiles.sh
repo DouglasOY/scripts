7d801333a87e6b6e76431439e4a622cde88ea87c
ouyangdonghai <ouyangdonghai@harmony.com>
2015-06-16 12:17:58 +0800

    update

--- awktwofiles.sh
+++ awktwofiles.sh
@@ -64,6 +64,11 @@ NR > FNR
             {
                 a[$1] = a[$1] / $2; 
             }
+            if ((a[x] < 0.5) || (a[x] > 2)) 
+            {
+                same[$1] = 0;
+                next;
+            }
             if ((a[x] < 0.8) || (a[x] > 1.2)) 
             {
                 tmp1 = 0;
@@ -83,6 +88,11 @@ NR > FNR
             {
                 b[$1] = b[$1] / $3; 
             }
+            if ((b[x] < 0.5) || (b[x] > 2)) 
+            {
+                same[$1] = 0;
+                next;
+            }
             if ((b[x] < 0.8) || (b[x] > 1.2)) 
             {
                 tmp2 = 0;
@@ -101,6 +111,11 @@ NR > FNR
             else
             {
                 c[$1] = c[$1] / $4; 
+            }
+            if ((c[x] < 0.5) || (c[x] > 2)) 
+            {
+                same[$1] = 0;
+                next;
             }
             if ((c[x] < 0.8) || (c[x] > 1.2)) 
             {
@@ -131,5 +146,4 @@ END
 compare standard
  
 
-awk ' NR == FNR { a[$1]=$2; b[$1]=$3; c[$1]=$4; standard[$1]=$0; } NR > FNR { compare[$1]=$0; same[$1] = 1; tmp1 = 1; tmp2 = 1; tmp3 = 1; for (x in a) { if (x == $1) { if ($2 == 0) { a[$1] = 1; } else { a[$1] = a[$1] / $2; } if ((a[x] < 0.8) || (a[x] > 1.2)) { tmp1 = 0; } } } for (x in b) { if (x == $1) { if ($3 == 0) { b[$1] = 1; } else { b[$1] = b[$1] / $3; } if ((b[x] < 0.8) || (b[x] > 1.2)) { tmp2 = 0; } } } for (x in c) { if (x == $1) { if ($4 == 0) { c[$1] = 1; } else { c[$1] = c[$1] / $4; } if ((c[x] < 0.8) || (c[x] > 1.2)) { tmp3 = 0; } } } if ((tmp1 == 0) && (tmp2 == 0) && (tmp3 == 0)) { same[$1] = 0; } } END { for (x in same) { if ( same[x] == 0 ) { print "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"; print compare[x]; print standard[x]; } } } ' compare standard
  
