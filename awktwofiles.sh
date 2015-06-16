9219367a2e07071af06653b38456c4791abef543
ouyangdonghai <ouyangdonghai@harmony.com>
2015-06-16 09:16:30 +0800

    update

--- awktwofiles.sh
+++ awktwofiles.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# file1
+# TimestampRead                    1.209     1.199     1.226
+# SemBCreate                       0.960     0.857     1.223
+# SemBDelete                       1.406     1.265     1.733
+
+# file2
+# TimestampRead                    1.210     1.199     1.226
+# SemBCreate                       0.960     0.860     1.250
+# SemBDelete                       1.402     1.277     1.655
+
+
 awk '
 NR == FNR 
 {
@@ -10,9 +21,9 @@ NR == FNR
 
 NR > FNR
 {
-    for (x in a) if (x == $1) { a[$1] = a[$1] / $2; }
-    for (x in b) if (x == $1) { b[$1] = b[$1] / $3; }
-    for (x in c) if (x == $1) { c[$1] = c[$1] / $4; }
+    for (x in a) if (x == $1) {a[$1] = a[$1] / $2;}
+    for (x in b) if (x == $1) {b[$1] = b[$1] / $3;}
+    for (x in c) if (x == $1) {c[$1] = c[$1] / $4;}
 }
 
 END
@@ -108,7 +119,7 @@ END
 {
     for (x in same) 
     { 
-        if ( same[x] == 0 ) 
+        if (same[x] == 0) 
         {
             print "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -";
             print compare[x];
@@ -119,8 +130,6 @@ END
 '
 compare standard
  
-
-
 
 awk ' NR == FNR { a[$1]=$2; b[$1]=$3; c[$1]=$4; standard[$1]=$0; } NR > FNR { compare[$1]=$0; same[$1] = 1; tmp1 = 1; tmp2 = 1; tmp3 = 1; for (x in a) { if (x == $1) { if ($2 == 0) { a[$1] = 1; } else { a[$1] = a[$1] / $2; } if ((a[x] < 0.8) || (a[x] > 1.2)) { tmp1 = 0; } } } for (x in b) { if (x == $1) { if ($3 == 0) { b[$1] = 1; } else { b[$1] = b[$1] / $3; } if ((b[x] < 0.8) || (b[x] > 1.2)) { tmp2 = 0; } } } for (x in c) { if (x == $1) { if ($4 == 0) { c[$1] = 1; } else { c[$1] = c[$1] / $4; } if ((c[x] < 0.8) || (c[x] > 1.2)) { tmp3 = 0; } } } if ((tmp1 == 0) && (tmp2 == 0) && (tmp3 == 0)) { same[$1] = 0; } } END { for (x in same) { if ( same[x] == 0 ) { print "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"; print compare[x]; print standard[x]; } } } ' compare standard
  
