5b8340965ac25b21b07070e9247ccc36b0b3fbf3
ouyangdonghai <ouyangdonghai@harmony.com>
2015-09-21 15:37:59 +0800

    update

--- getoptexample.sh
+++ getoptexample.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+#Set Script Name variable
+SCRIPT=`basename ${BASH_SOURCE[0]}`
+
+#Set fonts for Help.
+NORM=`tput sgr0`
+BOLD=`tput bold`
+REV=`tput smso`
+
+#Help function
+function HELP {
+  echo -e \\n"Help documentation for ${BOLD}${SCRIPT}.${NORM}"\\n
+  echo -e "${REV}Basic usage:${NORM} ${BOLD}$SCRIPT file.ext${NORM}"\\n
+  echo "Command line switches are optional. The following switches are recognized."
+  echo "${REV}-a${NORM}  --Sets the value for option ${BOLD}a${NORM}. Default is ${BOLD}A${NORM}."
+  echo "${REV}-b${NORM}  --Sets the value for option ${BOLD}b${NORM}. Default is ${BOLD}B${NORM}."
+  echo "${REV}-c${NORM}  --Sets the value for option ${BOLD}c${NORM}. Default is ${BOLD}C${NORM}."
+  echo "${REV}-d${NORM}  --Sets the value for option ${BOLD}d${NORM}. Default is ${BOLD}D${NORM}."
+  echo -e "${REV}-h${NORM}  --Displays this help message. No further functions are performed."\\n
+  echo -e "Example: ${BOLD}$SCRIPT -a foo -b man -c chu -d bar file.ext${NORM}"\\n
+  exit 1
+}
+
+#Check the number of arguments. If none are passed, print help and exit.
+NUMARGS=$#
+echo -e \\n"Number of arguments: $NUMARGS"
+if [ $NUMARGS -eq 0 ]; then
+  HELP
+fi
+
 # “a” and “arga” have optional arguments with default values.
 # “b” and “argb” have no arguments, acting as sort of a flag.
 # “c” and “argc” have required arguments.
@@ -36,3 +65,4 @@ done
 echo "ARG_A = $ARG_A"
 echo "ARG_B = $ARG_B"
 echo "ARG_C = $ARG_C"
+
