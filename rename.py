99dcea66c2056690f045cbf00b5670fdf3bb12ab
ouyangdonghai <ouyangdonghai@harmony.com>
2015-09-24 15:07:33 +0800

    update

--- rename.py
+++ rename.py
@@ -2,33 +2,27 @@
 # -*- coding: utf-8 -*-
 
 import os
-print os.getcwd()
-os.chdir("D:\\aa\\bb")
 
+directory = "D:\\aa\\bb"
 pattern = "ppppp"
 replacement = "rrrrr"
 
+os.chdir(directory)
 for i,j,k in os.walk('.'):
     if i == '.':
         files = k
 
+print os.getcwd()
 for f in files:
     pos = f.find(pattern)
     if pos == -1:
         continue
     
     prefix = f[:pos]
-    print prefix
-    
     suffix = f[(pos + len(pattern)):]
-    print suffix
-    
-    if not replacement:
-        newf = prefix + replacement + suffix
-    else:
-        newf = prefix + suffix
+    newf = prefix + replacement + suffix
         
     os.rename(f, newf)
     print f + " ==> " + newf
-    
+
 
