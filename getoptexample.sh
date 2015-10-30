4bc3def0dbdce596e2bbe9f33221de3d2625f798
ouyangdonghai <ouyangdonghai@harmony.com>
2015-10-30 09:59:55 +0800

    pretty getopt shell script

--- getoptexample.sh
+++ getoptexample.sh
@@ -1,48 +1,60 @@
 #!/bin/bash
 
-#Set Script Name variable
+#----------------------------------------
+# Set Script Name variable
+#----------------------------------------
 SCRIPT=`basename ${BASH_SOURCE[0]}`
 
-#Set fonts for Help.
+#----------------------------------------
+# Set fonts for Help.
+#----------------------------------------
 NORM=`tput sgr0`
 BOLD=`tput bold`
 REV=`tput smso`
 
-#Help function
+#----------------------------------------
+# Help function
+#----------------------------------------
 function HELP {
-  echo -e \\n"Help documentation for ${BOLD}${SCRIPT}.${NORM}"\\n
-  echo -e "${REV}Basic usage:${NORM} ${BOLD}$SCRIPT file.ext${NORM}"\\n
-  echo "Command line switches are optional. The following switches are recognized."
-  echo "${REV}-a${NORM}  --Sets the value for option ${BOLD}a${NORM}. Default is ${BOLD}A${NORM}."
-  echo "${REV}-b${NORM}  --Sets the value for option ${BOLD}b${NORM}. Default is ${BOLD}B${NORM}."
-  echo "${REV}-c${NORM}  --Sets the value for option ${BOLD}c${NORM}. Default is ${BOLD}C${NORM}."
-  echo "${REV}-d${NORM}  --Sets the value for option ${BOLD}d${NORM}. Default is ${BOLD}D${NORM}."
-  echo -e "${REV}-h${NORM}  --Displays this help message. No further functions are performed."\\n
-  echo -e "Example: ${BOLD}$SCRIPT -a foo -b man -c chu -d bar file.ext${NORM}"\\n
-  exit 1
+    echo "${REV}Usage:  ${NORM}       ${BOLD}${SCRIPT} [--arga=<ARGA>] --argb --argc=<ARGC> file.ext${NORM}"
+    echo ""
+    echo "${REV}Options:${NORM}"
+    echo "-a, --arga    the value for option arga, default is A"
+    echo "-b, --argb    the value for option argb, default is B"
+    echo "-c, --argc    the value for option argc, default is C"
+    echo "-h, --help    display this help message"
+    echo ""
+    echo "${REV}Example:${NORM}      ${BOLD}${SCRIPT} -afoo -b -cbar others${NORM}"
+    echo "              ${BOLD}${SCRIPT} --arga=foo --argb --argc=bar file.ext${NORM}"
+    exit 1
 }
 
-#Check the number of arguments. If none are passed, print help and exit.
-NUMARGS=$#
-echo -e \\n"Number of arguments: $NUMARGS"
-if [ $NUMARGS -eq 0 ]; then
-  HELP
-fi
+#----------------------------------------
+# If no arguments, print help and exit.
+#----------------------------------------
+if [ $# -eq 0 ]; then HELP; fi
 
+#----------------------------------------
+# read the options
+#----------------------------------------
+OPTTEMP=`getopt -o a::bc:h --long arga::,argb,argc:,help -n "${SCRIPT}" -- "$@"`
+if [ $? -ne 0 ] ; then echo "Incorrect options provided"; exit 1; fi
+eval set -- "${OPTTEMP}"
+
+#----------------------------------------
 # “a” and “arga” have optional arguments with default values.
 # “b” and “argb” have no arguments, acting as sort of a flag.
 # “c” and “argc” have required arguments.
-
 # set an initial value for the flag
-ARG_B=0
+#----------------------------------------
+ARG_A=default
+ARG_B=False
+ARG_C=NULL
+ARG_O=other
 
-# read the options
-TEMP=`getopt -o a::bc: --long arga::,argb,argc: -n 'getoptexample.sh' -- "$@"`
-# bad arguments
-if [ $? -ne 0 ] ; then echo "Incorrect options provided"; exit 1; fi
-eval set -- "$TEMP"
-
+#----------------------------------------
 # extract options and their arguments into variables.
+#----------------------------------------
 while true ; do
     case "$1" in
         -a|--arga)
@@ -50,19 +62,24 @@ while true ; do
                 "") ARG_A='some default value' ; shift 2 ;;
                 *) ARG_A=$2 ; shift 2 ;;
             esac ;;
-        -b|--argb) ARG_B=1 ; shift ;;
+        -b|--argb) ARG_B=True ; shift ;;
         -c|--argc)
             case "$2" in
                 "") shift 2 ;;
                 *) ARG_C=$2 ; shift 2 ;;
             esac ;;
-        --) shift ; break ;;
+        -h|--help) HELP ;;
+        --) ARG_O=$2; shift ; break ;;
         *) echo "Internal error!" ; exit 1 ;;
     esac
 done
 
+#----------------------------------------
 # do something with the variables -- in this case the lamest possible one :-)
-echo "ARG_A = $ARG_A"
-echo "ARG_B = $ARG_B"
-echo "ARG_C = $ARG_C"
+#----------------------------------------
+echo "ARG_A = ${ARG_A}"
+echo "ARG_B = ${ARG_B}"
+echo "ARG_C = ${ARG_C}"
+echo "ARG_O = ${ARG_O}"
+
 
