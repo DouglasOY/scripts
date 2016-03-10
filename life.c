fbfa5ab3d2ecbd4355262ca9a9db41ee919f9db7
ouyangdonghai <ouyangdonghai@harmony.com>
2016-03-10 11:35:13 +0800

    life

---
+++ life.c
@@ -0,0 +1,52 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int lottery = 0;
+    int total = 0;
+    int sum = 0;
+    int count = 0;
+
+    /* head -500000 /dev/urandom > p5000 */
+    FILE *fp = fopen("p5000", "rb");
+    if (fp == NULL)
+    {
+        perror("Failed to open file \n");
+        return EXIT_FAILURE;
+    }
+
+    while (1)
+    {
+        lottery = fgetc(fp);
+        if (lottery == EOF)
+        {
+            printf("EOF\n");
+            break;
+        }
+
+        total += 1;
+        count += 1;
+        if (total < 30000)
+        {
+            if (lottery <= 2) sum -= 1;
+            if (lottery >= 253) sum += 1;
+        }
+        else
+        {
+            printf("%d\n", sum);
+            sum = 0;
+            if (lottery <= 2) sum = -1;
+            if (lottery >= 253) sum = 1;
+            total = 1;
+        }
+    }
+
+    fclose(fp);
+    printf("count = [%d]\n", count);
+
+    return EXIT_SUCCESS;
+}
+
+
