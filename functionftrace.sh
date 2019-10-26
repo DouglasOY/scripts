fa95599c47a0c5e4861bc8a0b5483c679a81fe56
misty rain <ouyangdonghai@gmail.com>
2019-10-26 20:03:12 +0800

    Update functionftrace.sh

--- functionftrace.sh
+++ functionftrace.sh
@@ -1,9 +1,47 @@
+root@centos7:/home/cloud/ftrace/fork# cat functionftrace.sh 
 #!/bin/bash
 
 debugfs=/sys/kernel/debug
 echo nop > ${debugfs}/tracing/current_tracer
 echo 0 > ${debugfs}/tracing/tracing_on
-echo `pidof readfile.elf` > ${debugfs}/tracing/set_ftrace_pid
+echo `pidof haha.elf ` > ${debugfs}/tracing/set_ftrace_pid
 echo function_graph > ${debugfs}/tracing/current_tracer
-echo vfs_read > ${debugfs}/tracing/set_graph_function
+echo do_fork > ${debugfs}/tracing/set_graph_function
 echo 1 > ${debugfs}/tracing/tracing_on
+
+root@centos7:/home/cloud/ftrace/fork# cat fork.c 
+#include <unistd.h>
+#include <stdio.h> 
+#include <fcntl.h>
+
+int main (void) 
+{ 
+    pid_t fpid;
+    int count=0;
+
+    sleep(30);
+    fpid=fork(); 
+    if (fpid < 0) { 
+        printf("error in fork!"); 
+    }
+    else if (fpid == 0) {
+        printf("i am the child process, my process id is %d\n",getpid()); 
+        count++;
+    }
+    else {
+        printf("i am the parent process, my process id is %d\n",getpid()); 
+        count++;
+    }
+    printf("result: %d\n",count);
+    return 0;
+}
+
+root@centos7:/home/cloud/ftrace/fork# cat makefile 
+haha.elf: fork.o
+	gcc -o $@ $^
+
+fork.o : fork.c
+	gcc -o $@ -c $^
+
+clean:
+	rm *.elf *.o
